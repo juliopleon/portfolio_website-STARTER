7f95aa329fbb45a7e85bed0fc1035e490decf091
Julio Leon <juliopena143@icloud.com>
2023-02-21 17:17:55 -0700

    header done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => (
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: 'white' }}>
+          <DiCssdeck size="3rem" /> <span>Portfolio</span>
 
         </a>
       </Link>
