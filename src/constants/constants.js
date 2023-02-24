61c658d697538d3c671b062db90d5aa9f1e037d4
Julio Leon <juliopena143@icloud.com>
2023-02-23 23:28:55 -0700

    nice

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'Keepr',
     description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
     image: '/images/1.png',
-    tags: ['Mongo', 'Express', 'React', 'Node'],
+    tags: ['Mongo', 'Express', 'JavaScript', 'C#'],
     source: 'https://google.com',
     visit: 'https://google.com',
     id: 0,
